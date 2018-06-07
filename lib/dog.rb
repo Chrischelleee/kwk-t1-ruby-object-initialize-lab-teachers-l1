d0106c61c3db27932cc80b5183cbb2122bdbba8d
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-07 19:19:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,9 @@ class Dog
   def initialize(name, breed)
     @name = name
     @breed =  breed
+    if breed == " "
+      @breed = "Mutt"
+    end
   end
   def name=(name)
     @name = name
@@ -11,9 +14,6 @@ class Dog
   end
   def breed=(breed)
     @breed = breed
-    if breed == " "
-      @breed = "Mutt"
-    end
   end
   def breed(breed)
     @breed
